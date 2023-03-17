b1080504b7bd38adab9dc83e8c38d21e986e44e3
Falk Markus Dursun Jetlund <falk@jetlund.com>
2023-03-17 09:14:28 +0000

    Add lspconfig and mason

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,48 @@
+local settings = function()
+    local lspconfig = require 'lspconfig'
+    local lspformat = require 'lsp-format'
+    return {
+        tsserver = {
+            root_dir = lspconfig.util.root_pattern('package.json'),
+            single_file_support = false,
+        },
+        denols = {
+            root_dir = lspconfig.util.root_pattern('deno.json', 'deno.jsonc'),
+        },
+        default = {
+            on_attach = lspformat.on_attach
+        }
+    }
+end
+
+local config = function()
+    local lspconfig = require 'lspconfig'
+    local mason = require 'mason'
+    local mason_lspconfig = require 'mason-lspconfig'
+    local lsp_settings = settings()
+
+    mason.setup {}
+    mason_lspconfig.setup {}
+    mason_lspconfig.setup_handlers({
+        function(server)
+            local s = {}
+            for k, v in pairs(lsp_settings.default) do s[k] = v end
+            if lsp_settings[server] then
+                for k, v in pairs(lsp_settings[server]) do s[k] = v end
+            end
+            lspconfig[server].setup(s)
+        end
+    })
+end
+
+return {
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            'williamboman/mason.nvim',
+            'lukas-reineke/lsp-format.nvim',
+            'williamboman/mason-lspconfig.nvim',
+        },
+        config = config,
+    },
+}
