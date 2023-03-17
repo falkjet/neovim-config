2e639c33107f9ee4ae7c81551bdf696bc43d9c4e
Falk Markus Dursun Jetlund <falk@jetlund.com>
2023-03-17 09:41:42 +0000

    Add telescope-ui-select.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,16 @@
 return {
     {
         'nvim-telescope/telescope.nvim',
-        dependencies = {'nvim-lua/plenary.nvim'}
-    }
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+        dependencies = {
+            'telescope.nvim',
+        },
+        init = function()
+            local telescope = require "telescope"
+            telescope.load_extension("ui-select")
+        end
+    },
 }
