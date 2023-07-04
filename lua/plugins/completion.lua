5a1500a2f72b9f6dfdda51237ee57de56766cceb
Falk Markus Dursun Jetlund <falk@jetlund.com>
2023-07-04 18:19:21 +0200

    Deprioritize lsp snippets

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,6 +1,8 @@
 local config = function()
     local luasnip = require 'luasnip'
     local cmp = require 'cmp'
+    local types = require 'cmp.types'
+
     local on_tab = cmp.mapping(function()
         if luasnip.jumpable(-1) then
             luasnip.jump(-1)
@@ -14,6 +16,11 @@ local config = function()
             fallback()
         end
     end, { 'i', 's' })
+
+    local function deprioritize_snippet(entry1, entry2)
+        if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then return false end
+        if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then return true end
+    end
 
     return {
         preselect = false,
@@ -38,7 +45,24 @@ local config = function()
             { name = 'nvim_lsp' },
         }, {
             { name = 'buffer' },
-        })
+        }),
+        sorting = {
+            priority_weight = 2,
+            comparators = {
+                deprioritize_snippet,
+                cmp.config.compare.offset,
+                cmp.config.compare.exact,
+                cmp.config.compare.scopes,
+                cmp.config.compare.score,
+                cmp.config.compare.recently_used,
+                cmp.config.compare.locality,
+                cmp.config.compare.kind,
+                cmp.config.compare.sort_text,
+                cmp.config.compare.length,
+                cmp.config.compare.order,
+
+            }
+        }
     }
 end
 
