026db440564ffb05e5bd944ccd47b68b33c0af89
Falk Markus Dursun Jetlund <falk@jetlund.com>
2023-07-05 14:42:27 +0200

    Not load everything from friendly snippets

--- lua/snippets/init.lua
+++ lua/snippets/init.lua
@@ -1,3 +1,8 @@
 require 'snippets.tex'
 
-require 'luasnip.loaders.from_vscode'.lazy_load()
+require 'luasnip.loaders.from_vscode'.lazy_load {
+    paths = { "~/.local/share/nvim/lazy/friendly-snippets" },
+    include = {
+        "python",
+    }
+}
